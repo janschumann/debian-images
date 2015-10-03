5b37cbca0b55e4bdba5afb6ca805b6fd6b9977f5
Jan Schumann <js@schumann-it.com>
2015-10-03 20:37:53 +0200

    cleanup

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# determine debians release code name
 CODENAME=$(lsb_release -c | awk '{print $2}')
 
-echo ">> Removing old puppet"
-aptitude -y purge puppet puppet-common puppetlabs-release facter
-echo ">> Installing puppet master"
+# install puppet
 wget http://apt.puppetlabs.com/puppetlabs-release-pc1-${CODENAME}.deb
 dpkg -i puppetlabs-release-pc1-${CODENAME}.deb
 apt-get -y update
 apt-get -y install puppet-agent
 
-# add puppets new location to the PATH for everyone
+# puppet 4 has a new install localtion and its binaries are not in the path
+# so let´s add puppets new location to the PATH for everyone
 echo "export PATH=\"\${PATH}:/opt/puppetlabs/bin\"" > /etc/profile.d/puppet.sh
 chmod a+x /etc/profile.d/puppet.sh
 # add it right now, so we do not have to re-login
